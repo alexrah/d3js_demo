641d4224eeb797ef3438224d72672d13291340b5
alessandro stoppato <alexrah@gmail.com>
2018-03-18 23:20:02 +0100

    add cartographic data + dbase to csv data & liguria extract

---
+++ cartographic_sources/run_this_to_download_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_populated_places.zip
